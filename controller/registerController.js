fef4718a53ad2738e13ff4134e8b75ed3f3f3f6b
Anthony_dev <atozbasic@gmail.com>
2022-09-07 09:55:16 +0100

    moved valiation to module

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const {loginValidation } = require("../module/validation")
 
 const registerControllerPost = (req, res) => {
   const { error } = registerValidation(req.body);
