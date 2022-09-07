94cf0e3de7e04c4b80cb1cc8012971248af9f1e8
Albert55666 <albertonyishi1@gmail.com>
2022-09-07 10:00:09 +0100

    changes were made

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose")
-const bodyParser= require("body-parser")
-const cors = require("cors")
+const app = express();
 
+//routes
+const registerRoute = require("./route/register");
 
+// Route Middleware
+app.use("/api", registerRoute);
 
-mongoose.connect("mongodb://localhost/StackDB",{
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-},()=> console.log("connected to StackDB"))
-require('dotenv').config();
-
-// use area 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
-
-
-
-app.listen(4040,()=> console.log("server is runing"))
-
-
-
+app.listen(4040, () => console.log("server is runing"));
