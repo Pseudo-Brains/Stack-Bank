bbd651580da3cbaf67383ebac98e7d39aa828c87
Albert55666 <albertonyishi1@gmail.com>
2022-09-07 09:11:53 +0100

    changes were made

--- module/module.js
+++ module/module.js
@@ -1,1 +1,55 @@
-const express = require("express");
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  FirstName: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  LastName: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  Password: {
+    type: String,
+    required: true,
+    max: 1024,
+    min: 6,
+  },
+  Email: {
+    type: String,
+    required: true,
+    max: 1024,
+    min: 6,
+    unique: true,
+  },
+  Phone: {
+    type: String,
+    required: true,
+    max: 100,
+    min: 11,
+    unique: true,
+  },
+  AccountNumber: {
+    type: String,
+    required: true,
+    max: 1024,
+    min: 6,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = {
+  UserModel: mongoose.model("User", UserSchema),
+};
