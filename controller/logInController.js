70787f63c52f61a91b53865c8ff2c02db2678db6
Anthony_dev <atozbasic@gmail.com>
2022-09-21 16:45:24 +0100

    unknown commit

--- controller/logInController.js
+++ controller/logInController.js
@@ -26,12 +26,12 @@ const logIncontroller = async function (req, res) {
      if (!User) return res.status(400).send("wrong email try angin");
             
       const correctPassword = await bcrypt.compare(loginData.password,User.password)  
-        
+
         if (!correctPassword) return res.status(400).send("wrong password try angin");
      
       const token = jwt.sign({_id:User.id}, process.env.TOKEN_SECRET)
            
-      // await UserModel.updateOne({email:loginData.email},{token:token});
+      await UserModel.updateOne({email:loginData.email},{token:token});
       const userData={
         email:User.email,
         id:User._id 
