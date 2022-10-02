e8ab27a156aa04c545ad6c03c72a3dcce97815ac
Somto Jude <ugwujude42@gmail.com>
2022-10-02 16:31:22 +0100

    Worked on refix

--- route/home.js
+++
@@ -1,9 +0,0 @@
-const Route = require("express").Router()
-const {homeController} = require("../controller/homeController")
-const {Authtoken} = require("../middlewares/authUser")
-
-Route.get("/dashboard" , Authtoken, homeController);
-
-module.exports = {
-    dashboard: Route
-}
--- route/register.js
+++
@@ -1,8 +0,0 @@
-const route = require("express").Router();
-const { registerControllerPost } = require("../controller/registerController");
-
-route.post("/register", registerControllerPost);
-
-module.exports = {
-  registerRoute: route,
-};
